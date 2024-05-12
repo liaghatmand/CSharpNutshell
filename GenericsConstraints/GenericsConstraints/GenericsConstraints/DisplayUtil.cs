f5d9db292693cf118d6f7d36566df4bf9ff12158
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-05-12 14:27:07 +0330

    Add project as example of Generic Parameter Type Constraints

---
+++ GenericsConstraints/GenericsConstraints/GenericsConstraints/DisplayUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsConstraints
+{
+    //We use Generics if we have one functionality with diffrent paramater types
+    //We use <T> for class if we have too many generic methods
+    public class DisplayUtil
+    {
+        public void DisplayValue<T>(T value)
+            //where T : struct
+            //where T : class //any reference type
+            //where T : class? //nullable
+            //where T : notnull
+            //where T : class , new() // a reference type with default parameterless constructor
+            //where T : class, IPerson // It has to implement IPerson
+            //where T : Enum
+
+        {
+            Console.WriteLine($"The Value is: {value}");
+        }
+
+        //we can have two parameters
+        //for every parameter we can have one constraint (where)
+        public void DisplayValue<T,U>(T value, U SecondValue)
+            where T : struct
+            where U : class
+
+        {
+            Console.WriteLine($"The Value is: {value}");
+            Console.WriteLine($"The Value is: {SecondValue}");
+        }
+    }
+}
---
+++ GenericsConstraints/GenericsConstraints/GenericsConstraints/Program.cs
@@ -0,0 +1,20 @@
+﻿using GenericsConstraints;
+
+int? intValue = 0;
+var person = new Person();
+int[] array = [];
+
+var instance_1 = new DisplayUtil();
+instance_1.DisplayValue(intValue);
+
+var instance_2 = new DisplayUtil();
+instance_2.DisplayValue("Zahra");
+
+var instance_3 = new DisplayUtil();
+instance_3.DisplayValue(Gender.female);
+
+var instance_4 = new DisplayUtil();
+instance_3.DisplayValue(person);
+
+var  instance_5 = new DisplayUtil();
+instance_5.DisplayValue(array);
